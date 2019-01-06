c95709ea08eac02ba759ee95d2e7418642cc225e
Shruti <sjagwani31@gmail.com>
2019-01-06 12:51:08 +0530

    Files Uploaded

---
+++ src/javaconnect.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Shruti
+ */
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+public class javaconnect {
+    Connection conn=null;
+    public static Connection ConnectDb(){
+        try{    
+            Class.forName("org.sqlite.JDBC");
+            Connection conn=DriverManager.getConnection("jdbc:sqlite:G:\\Java projects\\Banking_Management_System\\bank.sqlite");
+            return conn;
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,e);
+            
+        }
+        return null;
+    }
+}
