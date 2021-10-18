c85bfaacee78dca16feda0115241adbe61eaf3f2
Sarthak Agarwal <sarthak.20208110@mnnit.ac.in>
2021-10-18 15:34:59 +0530

    Clear button fix
    Fixes issue#18

--- src/Account.java
+++ src/Account.java
@@ -369,9 +369,6 @@ public class Account extends javax.swing.JFrame {
 
     private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
         // TODO add your handling code here:
-        jTextField1.setText(""); 
-        jTextField2.setText(""); 
-        jTextField3.setText(""); 
         jTextField4.setText(""); 
         jTextField5.setText(""); 
         jTextField6.setText(""); 
